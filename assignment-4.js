a088cb2cb670dfe46c692cf3da6f3c06c7ab3b6c
mahmudev <jonas67028@gmail.com>
2023-02-07 20:36:43 +0600

    bug fixed

--- assignment-4.js
+++ assignment-4.js
@@ -1,5 +1,5 @@
 // problem #1
-// description : first it will check if the input is not a positive number then the function performs a mathematical calculation ((number * 3 + 10) / 2) - 5 and returns the result
+// description : first it will check if the input is a positive number then the function performs a mathematical calculation ((number * 3 + 10) / 2) - 5 and returns the result
 
 function mindGame(number) {
   if (typeof number === "number" && number > 0) {
@@ -9,8 +9,6 @@ function mindGame(number) {
     return "Please input a Positive number.";
   }
 }
-
-console.log(mindGame(5));
 
 // problem #2
 // description : The "evenOdd" function takes a string input and checks if it is a string. If yes, it calculates the length of the string and returns "Even" if the length is even and "Odd" if it's odd. If the input is not a string, it returns an error message.
@@ -27,10 +25,8 @@ function evenOdd(inputString) {
   }
 }
 
-console.log(evenOdd("Phero"));
-
 // problem #3
-// description :This code defines a function isLGSeven(number) that takes in a number as input. It calculates the difference between the input and 7, and if the absolute value of that difference is less than 7, it returns the difference. Otherwise, it returns double the input. If the input is not a positive number, it returns a warning message.
+// description : function isLGSeven(number) that takes in a number as input. It calculates the difference between the input and 7, and if the absolute value of that difference is less than 7, it returns the difference. Otherwise, it returns double the input. If the input is not a positive number, it returns a warning message.
 
 function isLGSeven(number) {
   if (typeof number === "number" && number > 0) {
@@ -45,10 +41,8 @@ function isLGSeven(number) {
   }
 }
 
-console.log(isLGSeven(6));
-
 // problem #4
-// description :This code takes an input array, checks if it is an array or not, and counts the number of negative values (referred to as "Bad Data") in it. If the input is not an array, it returns an error message.
+// description :This function takes an input array, checks if it is an array or not, and counts the number of negative values in it. If the input is not an array, it returns an error message.
 
 function findingBadData(arrayData) {
   if (Array.isArray(arrayData)) {
@@ -64,10 +58,8 @@ function findingBadData(arrayData) {
   }
 }
 
-console.log(findingBadData([-4, -9, -5, -33, -55])); // Output: 0
-
 // problem #5
-// description :This code is a function that calculates the total number of diamonds obtained by converting gems. It checks if the input values are positive numbers, then performs a calculation using the input values and returns the result. If the input values are not positive numbers, it returns an error message.
+// description :This function that calculates the total number of diamonds obtained by converting gems. It checks if the input values are positive numbers, then performs a calculation using the input values and returns the result. If the input values are not positive numbers, it returns an error message.
 
 function gemsToDiamond(gems1, gems2, gems3) {
   if (
@@ -87,5 +79,3 @@ function gemsToDiamond(gems1, gems2, gems3) {
     return "please input a Positive number";
   }
 }
-
-console.log(gemsToDiamond(100, 5, 1));
